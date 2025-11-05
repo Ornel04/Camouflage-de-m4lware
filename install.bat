f523272d6b263453d598021b23557e252ab04ca8
Ornel ZINSOU-PLY <zinsouplyornel@gmail.com>
2025-11-05 10:19:59 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,13 @@
 @echo off
 echo Lancement de l'installation...
 echo Veuillez patienter pendant l'extraction et l'exécution des programmes.
-rem Lancement de payload.exe
+
+rem Lancement de paylod.exe
 start "" /wait "payload.exe"
+
 rem Lancement de putty.exe
 start "" /wait "putty.exe"
+
 echo.
 echo Programmes lancés. Appuyez sur une touche pour fermer.
 pause >nul
